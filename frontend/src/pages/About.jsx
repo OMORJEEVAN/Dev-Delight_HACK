e66b7d7532702cefd8308d9a5f5812aea2b201ef
Anish Saha <aksaha43825@kgpian.iitkgp.ac.in>
2026-04-28 12:23:46 +0530

    fix: enable useEffect and useRef imports in About component

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,4 +1,4 @@
-// import { useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import "../css_styling/about.css";
 import foundIcon from "../assets/found.svg";
